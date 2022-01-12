67f9e01982b606cabeaf62497e998ec04bc8fe4a
Damilare Jolayemi <djolayemi@gmail.com>
2022-01-12 21:59:40 +0100

    screen

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:my_shop/providers/product.dart';
+import 'package:my_shop/providers/products_provider.dart';
+import 'package:my_shop/widgets/app_drawer.dart';
+import 'package:my_shop/widgets/user_product_item.dart';
+import 'package:provider/provider.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  const UserProductsScreen({Key? key}) : super(key: key);
+
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Products'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add), onPressed: (){},
+          )
+        ],
+      ),
+        drawer: AppDrawer(),
+        body: Padding(
+           padding: EdgeInsets.all(8.0),
+            child: ListView.builder(
+              itemCount: productsData.items.length,
+              itemBuilder: (_, i) {
+                return Column(
+                  children: [
+                    UserProductItem(
+                      productsData.items[i].title,
+                      productsData.items[i].imageUrl,
+                    ),
+                    Divider()
+                  ],
+                );
+              },),
+    ),
+    );
+  }
+}
