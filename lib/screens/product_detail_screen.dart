c08314b3b5d673e29fc69cc1f74aaaecf12b99c8
Damilare Jolayemi <djolayemi@gmail.com>
2022-01-12 22:00:52 +0100

    product_detail

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_shop/providers/products_provider.dart';
+import 'package:provider/provider.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+
+  // final String title;
+  // ProductDetailScreen(this.title);
+
+  static const routeName = '/product-detail';
+  @override
+  Widget build(BuildContext context) {
+   final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context, listen: false).findById(productId);
+    return Scaffold(
+      appBar: AppBar(title: Text(loadedProduct.title)),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                width: double.infinity,
+                height: 300,
+                child: Image.network(
+                  loadedProduct.imageUrl,
+                  fit: BoxFit.cover,)
+                ),
+              SizedBox(height: 10,),
+              Text('\$${loadedProduct.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),),
+              SizedBox(height: 10,),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: Text(
+                    '${loadedProduct.desc}',
+                    textAlign: TextAlign.center,
+                    softWrap: true,)
+              )
+            ],
+          ),
+        ),
+    );
+  }
+}
