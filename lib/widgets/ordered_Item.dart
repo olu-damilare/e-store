89770c631990fdc7804ff4baf0eebb0980dc52e0
Damilare Jolayemi <djolayemi@gmail.com>
2022-01-12 21:56:19 +0100

    ordered item

---
+++ lib/widgets/ordered_Item.dart
@@ -0,0 +1,72 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:my_shop/providers/orders.dart';
+
+class OrderedItem extends StatefulWidget {
+
+  final OrderItem order;
+
+  OrderedItem(this.order);
+
+  @override
+  _OrderedItemState createState() => _OrderedItemState();
+}
+
+class _OrderedItemState extends State<OrderedItem> {
+
+  bool _expanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          ListTile(title: Text('\$${widget.order.amount}'),
+            subtitle: Text(DateFormat('dd MM yyyy hh:mm').format(widget.order.dateTime)
+            ),
+            trailing: IconButton(
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: (){
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+            ),
+          ),
+          if(_expanded) Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: min((widget.order.products.length * 20 + 15), 100),
+              child: ListView(children:
+                widget.order.products.map((prod) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget> [
+                    Text(
+                      prod.title,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    ),
+                    Text('${prod.quantity}x  \$${prod.price}',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.black54,
+                      fontWeight: FontWeight.w600
+                    ),
+                    )
+                  ],
+                )
+                ).toList()
+
+              )
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
