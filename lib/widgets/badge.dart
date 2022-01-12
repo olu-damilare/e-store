b80da917cb69b0f795c91cf9ea3c393011719f8b
Damilare Jolayemi <djolayemi@gmail.com>
2022-01-12 21:56:40 +0100

    badge

---
+++ lib/widgets/badge.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class Badge extends StatelessWidget {
+  Badge({Key? key, required this.child, required this.value, this.color}) : super(key: key);
+
+  final Widget child;
+  final String value;
+  Color? color;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        child,
+        Positioned(
+          right: 8,
+            top: 8,
+          child: Container(
+            padding: EdgeInsets.all(2.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              // ignore: unnecessary_null_comparison
+              color: color != null ? color : Theme.of(context).accentColor,
+            ),
+            constraints: BoxConstraints(
+              minWidth: 16,
+              minHeight: 16,
+            ),
+            child: Text(
+              value,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 10,
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
