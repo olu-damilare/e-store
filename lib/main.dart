e46763bd4c9140a9611c51edc87092f4fbb63f1e
Damilare Jolayemi <djolayemi@gmail.com>
2022-01-12 22:01:09 +0100

    main

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:my_shop/providers/cart.dart';
+import 'package:my_shop/providers/orders.dart';
+import 'package:my_shop/providers/products_provider.dart';
+import 'package:my_shop/screens/cart_screen.dart';
+import 'package:my_shop/screens/orders_screen.dart';
+import 'package:my_shop/screens/product_detail_screen.dart';
+import 'package:my_shop/screens/product_overview_screen.dart';
+import 'package:my_shop/screens/user_products_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  MultiProvider(providers: [
+        ChangeNotifierProvider(
+        create: (BuildContext context) => Products(),
+        ),
+      ChangeNotifierProvider(
+        create: (BuildContext context) => Cart(),
+      ),
+      ChangeNotifierProvider(
+        create: (BuildContext context) => Orders(),
+      )
+
+    ],
+      child: MaterialApp(
+        title: 'Slim Daddy\'s shop',
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.amber,
+            fontFamily: 'Abhaya'
+        ),
+        home: ProductOverviewScreen(),
+        routes:  {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          CartScreen.routeName: (ctx) => CartScreen(),
+          OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen()
+        },
+      ),
+    );
+  }
+}
+
+
+
