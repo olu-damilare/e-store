7bffbd696263fcb6536fa829e1e7e5b234d1ae6b
olu-damilare <djolayemi@gmail.com>
2022-02-08 22:49:26 +0100

    update

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
 
 class Product with ChangeNotifier{
   final String id;
@@ -18,8 +21,27 @@ class Product with ChangeNotifier{
   }
     );
 
-  void toggleFavouriteStatus(){
+  void _setFavValue(bool newValue) {
+    isFavourite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavouriteStatus() async{
+    final oldStatus = isFavourite;
     isFavourite = !isFavourite;
     notifyListeners();
+    final url = 'https://flutter-project-4fd4f-default-rtdb.firebaseio.com/products/$id.json';
+    try {
+     final response = await patch(Uri.parse(url), body: json.encode({
+        'isFavourite': isFavourite
+      }));
+
+     if(response.statusCode >= 400){
+       _setFavValue(oldStatus);
+     }
+    }catch(error){
+      _setFavValue(oldStatus);
+    }
+    
   }
 }
