635754773df3dc1a3db504af4f9e369fd48ced4b
olu-damilare <djolayemi@gmail.com>
2022-02-09 18:52:14 +0100

    update

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -10,16 +10,42 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
+    // final orderData = Provider.of<Orders>(context);
+   print('building orders');
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Orders"),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.allOrders.length,
-          itemBuilder: (ctx, i) => OrderedItem(orderData.allOrders[i]),
-      ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+        builder: (ctx, data) {
+         if(data.connectionState == ConnectionState.waiting){
+           return Center(
+               child: CircularProgressIndicator()
+           );
+         }else{
+           if(data.error != null){
+              return AlertDialog(
+                title: Text('An error occurred!'),
+                content: Text('Something went wrong.'),
+                actions: [
+                  FlatButton(onPressed: (){
+                    Navigator.of(ctx).pop();
+                  },
+                      child: Text('Okay')
+                  )
+                ],
+              );
+           }else{
+              return Consumer<Orders>(builder: (ctx, orderData, child) => ListView.builder(
+                itemCount: orderData.allOrders.length,
+                itemBuilder: (ctx, i) => OrderedItem(orderData.allOrders[i]),
+              )
+              );
+           }
+         }
+        })
     );
   }
 }
