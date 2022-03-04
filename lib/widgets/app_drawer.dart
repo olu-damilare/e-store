83609827c55d498da716c41aa266a967b5ec8a39
olu-damilare <djolayemi@gmail.com>
2022-03-05 00:54:35 +0100

    update

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/helpers/custom_route.dart';
+import 'package:my_shop/providers/auth.dart';
 import 'package:my_shop/screens/orders_screen.dart';
 import 'package:my_shop/screens/user_products_screen.dart';
+import 'package:provider/provider.dart';
 
 class AppDrawer extends StatelessWidget {
 
@@ -27,7 +30,10 @@ class AppDrawer extends StatelessWidget {
             leading: Icon(Icons.payment),
             title: Text('Orders'),
             onTap: (){
-              Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName);
+              Navigator.of(context).pushReplacement(
+                CustomRoute(builder: (ctx) => OrdersScreen())
+              );
+              // Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName);
             },
           ),
           Divider(),
@@ -37,6 +43,18 @@ class AppDrawer extends StatelessWidget {
             onTap: (){
               Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
             },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: (){
+              Navigator.of(context).pop();
+              Navigator.of(context).pushReplacementNamed('/');
+            //   Navigator.of(context).pushReplacementNamed(UserProductsScreen.routeName);
+              Provider.of<Auth>(context, listen: false).logout();
+
+              },
           )
         ],
       )
