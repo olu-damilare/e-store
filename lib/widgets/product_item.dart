fb6c733156cc8fd131b492134797983e2cde2793
olu-damilare <djolayemi@gmail.com>
2022-03-05 00:53:32 +0100

    update

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:my_shop/providers/auth.dart';
 import 'package:my_shop/providers/cart.dart';
 import 'package:my_shop/providers/product.dart';
 import 'package:my_shop/screens/product_detail_screen.dart';
@@ -17,6 +18,7 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
    final product =  Provider.of<Product>(context, listen: false);
    final cart = Provider.of<Cart>(context, listen: false);
+   final authData = Provider.of<Auth>(context, listen: false);
     return ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: GridTile(
@@ -25,9 +27,17 @@ class ProductItem extends StatelessWidget {
               Navigator.of(context).pushNamed(
                   ProductDetailScreen.routeName, arguments: product.id);
             },
-            child: Image.network(
-                product.imageUrl,
-                fit: BoxFit.cover)
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder: AssetImage('assets/images/[rpdict-placejolder.png'),
+
+                image: NetworkImage(
+                    product.imageUrl,
+                ),
+                fit: BoxFit.cover,
+              ),
+            )
           ),
             footer: GridTileBar(
               leading:
@@ -37,7 +47,7 @@ class ProductItem extends StatelessWidget {
                       Icons.favorite : Icons.favorite_border_sharp
                   ),
                 onPressed: () {
-                    product.toggleFavouriteStatus();
+                    product.toggleFavouriteStatus(authData.token as String, authData.userId as String);
                 },
                 color: Theme.of(context).accentColor,
               ),
