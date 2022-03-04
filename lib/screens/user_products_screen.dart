13783223600082d81a73f14e9b8b6bc2f07267f8
olu-damilare <djolayemi@gmail.com>
2022-03-05 00:54:52 +0100

    update

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -13,7 +13,7 @@ class UserProductsScreen extends StatelessWidget {
   static const routeName = '/user-products';
 
   Future<void> _refreshProducts(BuildContext context) async{
-    await Provider.of<Products>(context, listen: false).fetchProducts();
+    await Provider.of<Products>(context, listen: false).fetchProducts(true);
 
   }
 
@@ -34,25 +34,34 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
         drawer: AppDrawer(),
-        body: RefreshIndicator (
-          onRefresh: () => _refreshProducts(context),
-          child: Padding(
-             padding: EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: productsData.items.length,
-                itemBuilder: (_, i) {
-                  return Column(
-                    children: [
-                      UserProductItem(
-                        productsData.items[i].id,
-                        productsData.items[i].title,
-                        productsData.items[i].imageUrl,
-                      ),
-                      Divider()
-                    ],
-                  );
-                },),
+        body: FutureBuilder(
+          future: _refreshProducts(context),
+          builder: (ctx, snapshot) => snapshot.connectionState == ConnectionState.waiting ?
+          Center(
+            child: CircularProgressIndicator(),
+          )
+          : RefreshIndicator (
+            onRefresh: () => _refreshProducts(context),
+            child: Consumer<Products>(
+              builder: (ctx, productsData, _) => Padding(
+                 padding: EdgeInsets.all(8.0),
+                  child: ListView.builder(
+                    itemCount: productsData.items.length,
+                    itemBuilder: (_, i) {
+                      return Column(
+                        children: [
+                          UserProductItem(
+                            productsData.items[i].id,
+                            productsData.items[i].title,
+                            productsData.items[i].imageUrl,
+                          ),
+                          Divider()
+                        ],
+                      );
+                    },),
     ),
+            ),
+          ),
         ),
     );
   }
